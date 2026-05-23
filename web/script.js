c625bde3c41dfe90eb74fd58fd0c9b4e1283ba73
unknown <gusttavo11bona@gmail.com>
2026-05-23 14:40:18 -0300

    AlterandoJS

--- web/script.js
+++ web/script.js
@@ -1,5 +1,4 @@
 async function gerarPDF() {
-
   const dados = {
     data: document.getElementById("data").value,
     nome: document.getElementById("nome").value,
@@ -9,24 +8,19 @@ async function gerarPDF() {
     cpf: document.getElementById("cpf").value
   };
 
-  const resposta = await fetch("http://localhost:3000/gerar", {
+  const resposta = await fetch("https://termos-1gbi.onrender.com/gerar", {
     method: "POST",
-
     headers: {
       "Content-Type": "application/json"
     },
-
     body: JSON.stringify(dados)
   });
 
   const blob = await resposta.blob();
-
   const url = window.URL.createObjectURL(blob);
 
   const a = document.createElement("a");
-
   a.href = url;
   a.download = "termo.pdf";
-
   a.click();
 }
